da713f63a14d89716e3cc516e1f94f3a55db793a
Sangjin Han <xmfl34@naver.com>
2022-07-21 16:22:16 +0900

    production.py modified

--- config/production.py
+++ config/production.py
@@ -1,5 +1,11 @@
 from logging.config import dictConfig
 
+from config.default import *
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
+
 dictConfig({
     'version': 1,
     'formatters': {
@@ -12,14 +18,14 @@ dictConfig({
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
-            'maxBytes': 1024 * 1024 * 5, # 5 MB
+            'maxBytes': 1024 * 1024 * 5,    # 5 MB
             'backupCount': 5,
             'formatter': 'default',
         },
     },
     'root': {
         'level': 'INFO',
-        'handelers': ['file']
+        'handlers': ['file']
     }
 })
 
